86528a61ad7bc24787d5c1316c8cbe797bda043b
eamonnk <eamonn_kelly@hotmail.com>
2024-10-01 12:15:32 +0100

    updates

---
+++ py-files/hand-off/week1-exercise1-print-shape.py
@@ -0,0 +1,12 @@
+# Sept 2024
+# Exercise 1 - Write a Python script that produces the following output on the console:
+# **********
+# *        *
+# *        *
+# *        *
+# **********
+
+# -- SOLUTION --
+# Using print command to display square shape composed of asterisks and \n escape sequence to wrap to a new line to allow output 
+# to be completed in a single print function rather than multiple print commands, making it more efficient
+print("**********\n*        *\n*        *\n*        *\n**********")
